a6f9feab4383c5b6c58fa4cd490019490996e0ad
KIMYONGGEE <ykkim6872@gmail.com>
2019-11-27 03:10:48 +0900

    First Version 1. Create DB 2. Compare with  DB 3. If login is successful, go to TFL Image Processing.

---
+++ app/src/main/java/com/delaroystudios/sqlitelogin/activities/UsersActivity.java
@@ -0,0 +1,43 @@
+package com.delaroystudios.sqlitelogin.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.delaroystudios.sqlitelogin.R;
+
+/**
+ * Created by delaroy on 3/27/17.
+ */
+public class UsersActivity extends AppCompatActivity {
+
+    private TextView textViewName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+
+        textViewName = (TextView) findViewById(R.id.text1);
+        String nameFromIntent = getIntent().getStringExtra("Name");
+        textViewName.setText("Welcome " + nameFromIntent);
+
+        Log.d(this.getClass().getName(), "잘눌림 ");
+
+        Handler delayHandler = new Handler();
+        delayHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = getPackageManager().getLaunchIntentForPackage("org.tensorflow.lite.examples.classification");
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        }, 1000);
+
+
+    }
+}
